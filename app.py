54debeff01f568d6e69f8a391f7b1b0be78ea812
Пятина Дарья <dora628962@gmail.com>
2025-09-14 22:15:29 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
 @app.route("/web")
-def start():
+def web():
     return '''<!doctype html>
         <html>
            <body> 
                <h1>web-сервер на flask</h1>
-           <body> 
+               <a href="/author">author</a>
+           </body> 
         </html>'''
+
+@app.route("/author")
+def author():  
+    name = "Пятина Дарья Вадимовна"
+    group = "ФБИ-31"
+    faculty = "ФБ"
+
+    return """<!doctype html>
+        <html>
+            <body>
+                <p>Студент: """ + name + """</p>
+                <p>Группа: """ + group + """</p>
+                <p>Факультет: """ + faculty + """</p>
+                <a href="/web">web</a>
+            </body>
+        </html>"""
