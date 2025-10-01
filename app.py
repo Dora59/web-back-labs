55c91abaa45888a1cb8c21b65b6e15089a312bf5
Пятина Дарья <dora628962@gmail.com>
2025-10-01 23:09:02 +0700

    Имя студента

--- app.py
+++ app.py
@@ -378,4 +378,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Пятина Дарья'
+    return render_template('example.html', name=name)
