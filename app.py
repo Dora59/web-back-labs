8d261eb9e8319b32cfad20831b0b5eaecee3443e
Пятина Дарья <dora628962@gmail.com>
2025-10-12 02:47:50 +0700

    Наследование шаблонов

--- app.py
+++ app.py
@@ -390,3 +390,6 @@ def example():
                            name=name, lab_num=lab_num, number=number,
                            group=group, course=course, fruits=fruits)
 
+@app.route('/lab2/')
+def lab2():
+    return render_template('lab2.html')
