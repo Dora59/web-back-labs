666b1aed27feb59e7521bda5d49fdaffa32fe65d
Пятина Дарья <dora628962@gmail.com>
2025-10-18 17:40:25 +0700

    Рефакторинг: выделение второй лабораторной

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, url_for, request, redirect, abort, render_template
 from lab1 import lab1
+from lab2 import lab2 
 import datetime 
 
 app = Flask(__name__)
 app.register_blueprint(lab1) 
+
+app = Flask(__name__)
+app.register_blueprint(lab2) 
+
 
 @app.route("/")
 @app.route("/index")
@@ -214,5 +219,4 @@ def internal_server_error(err):
     </body>
 </html>
 ''', 500
-
 
